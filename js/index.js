d144256e297a4b510e9457c9d06cbda26a96c6e6
Angel Torres <alas4224@gmail.com>
2019-03-21 00:15:54 -0500

    add navbar opacity change effect

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+const navbar = document.querySelector(".banner");
+
+this.addEventListener("scroll", function(e) {
+    const scroll = document.documentElement.scrollTop
+    if (scroll > 0) {
+        document.querySelector(".navbar").style.opacity = ".6";
+    } else {
+        document.querySelector(".navbar").style.opacity = "1";
+    }
+});
